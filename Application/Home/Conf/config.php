5373583b638372ca6695c1405fcce1cb579d0351
boolean93 <boolean93@163.com>
2014-12-23 02:46:11 +0800

    commit nothing

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -11,11 +11,11 @@ return array(
     //数据库配置
 	'DB_TYPE'               =>  'pdo',     		// 数据库类型
     'DB_HOST'               =>  'localhost', 		// 服务器地址
-    'DB_NAME'               =>  'timetravel',       // 数据库名
-    'DB_USER'               =>  'root',      		// 用户名
-    'DB_PWD'                =>  '',    	    	    // 密码
+    'DB_NAME'               =>  'travel_travel',       // 数据库名
+    'DB_USER'               =>  'travel',      		// 用户名
+    'DB_PWD'                =>  'traveltraveltravel',    	    	    // 密码
     'DB_PREFIX'   			=>	'',					//表前缀
-    'DB_DSN'    => 'mysql:host=localhost;dbname=timetravel;charset=utf8',
+    'DB_DSN'    => 'mysql:host=localhost;dbname=travel_travel;charset=utf8',
 
     'PASSWORD_SALT'         =>  "Hey,TimeTravel",
 
