dcc57ab35d35824bfa8a737b9b56263cea389412
boolean93 <boolean93@163.com>
2014-12-28 18:27:38 +0800

    install QC

---
+++ Connect2.1/API/comm/inc.php
@@ -0,0 +1,2 @@
+<?php die('forbidden'); ?>
+{"appid":"101181780","appkey":"0abaf788b0cd028b0d250dbb5852af0f","callback":"http://sglx16.com","scope":"get_user_info","errorReport":true,"storageType":"file","host":"localhost","user":"root","password":"root","database":"test"}
