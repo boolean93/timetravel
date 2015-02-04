72eac30a055b2054cf389d38b2374ca79df05bdd
boolean93 <boolean93@163.com>
2015-02-05 00:15:38 +0800

    hey

--- Application/Home/Controller/BaseController.class.php
+++ Application/Home/Controller/BaseController.class.php
@@ -15,9 +15,14 @@ class BaseController extends Controller{
         //淘宝店绑定
         $taobao = M("Taobao")->find(1);
         $ercode = M("Ercode")->find(1);
+        $contact = M("Contact")->find(1);
 
         $this->assign('taobao', $taobao);
+
         $this->assign('er1', $ercode['url1']);
         $this->assign('er2', $ercode['url2']);
+
+        $this->assign("qq", $contact["qq"]);
+        $this->assign("tel", $contact["tel"]);
     }
 }
