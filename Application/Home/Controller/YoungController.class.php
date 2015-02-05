f23f15aa0d56ed3af533e7ec9442b5d733c4f6b6
boolean93 <boolean93@163.com>
2015-02-06 00:30:57 +0800

    Young - Added some comments in order to make me understand them in the future.

--- Application/Home/Controller/YoungController.class.php
+++ Application/Home/Controller/YoungController.class.php
@@ -17,22 +17,27 @@ class YoungController extends BaseController {
 
         $temp = array();
         $res = array();
+        //整理时间格式
         for($i = 0; $i < count($older); $i++ ){
             $year = date('Y', $older[$i]['end_time']);
             $temp[$year][] = $older[$i];
         }
 
+        //将数据分组并把年份分离开
         foreach($temp as $k => $v){
             $res[$k] = divideInto($v, 4);
             $resYear[] = $k;
         }
 
+        //翻转数组, 因为js实现的时候是反着的T_T
         foreach($res as $k => $v){
             $res[$k] = array_reverse($res[$k]);
         }
 
+        //倒序结果
         $res = array_reverse($res);
         $resYear = array_reverse($resYear);
+
         $this->assign("older", $res);
         $this->assign("year", $resYear);
         $this->assign("latest", $latest);
